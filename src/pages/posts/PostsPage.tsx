8cd9ba868e50a24091057b999dc23e5b35413bf0
minhcu <minhdong1831999@gmail.com>
2025-11-13 20:16:24 +0700

    add: CI

--- src/pages/posts/PostsPage.tsx
+++ src/pages/posts/PostsPage.tsx
@@ -4,106 +4,125 @@ import { PostList } from "./post-list/post-list.tsx";
 import { useEffect, useMemo, useState } from "react";
 import { type PostData, posts } from "./shared/data/posts.data.ts";
 
-
 export interface DropdownOption {
-  value: string;
-  label: string;
+    value: string;
+    label: string;
 }
 
 const SORT_OPTIONS: DropdownOption[] = [
-  { value: "Newest First", label: "Newest First" },
-  { value: "Oldest First", label: "Oldest First" },
-  { value: "Title A - Z", label: "Title A - Z" },
+    { value: "Newest First", label: "Newest First" },
+    { value: "Oldest First", label: "Oldest First" },
+    { value: "Title A - Z", label: "Title A - Z" },
 ] as const;
 
 export const PostsPage = () => {
-  const [searchValue, setSearchValue] = useState<string>("");
-  const [selectedCategory, setSelectedCategory] = useState<DropdownOption>({
-    value: "All Categories",
-    label: "All Categories",
-  });
-  const [selectedAuthors, setSelectedAuthors] = useState<DropdownOption>({
-    value: "All Authors",
-    label: "All Authors",
-  });
-  const [selectedNews, setSelectedNews] = useState<DropdownOption>({
-    value: "Newest First",
-    label: "Newest First",
-  });
-  const [categoryOptions, setCategoryOptions] = useState<DropdownOption[]>(
-    []
-  );
-  const [authorOptions, setAuthorOptions] = useState<DropdownOption[]>([]);
-
-  const filteredPosts = useMemo(() => {
-    return posts.filter((post) => {
-      const matchesSearch = post.title
-        .toLowerCase()
-        .includes(searchValue.toLowerCase());
-      const matchesCategory =
-        selectedCategory.value === "All Categories" ||
-        selectedCategory.value === post.categories;
-      const matchesAuthor =
-        selectedAuthors.value === "All Authors" ||
-        selectedAuthors.value === post.author;
-      return matchesSearch && matchesCategory && matchesAuthor;
-    }).sort((a, b) => {
-      if (selectedNews.value === "Newest First") {
-        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-      } else if (selectedNews.value === "Oldest First") {
-        return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
-      } else if (selectedNews.value === "Title A - Z") {
-        return a.title.localeCompare(b.title);
-      }
-      return 0;
+    const [searchValue, setSearchValue] = useState<string>("");
+    const [selectedCategory, setSelectedCategory] = useState<DropdownOption>({
+        value: "All Categories",
+        label: "All Categories",
     });
-  }, [searchValue, selectedCategory, selectedAuthors, selectedNews]);
+    const [selectedAuthors, setSelectedAuthors] = useState<DropdownOption>({
+        value: "All Authors",
+        label: "All Authors",
+    });
+    const [selectedNews, setSelectedNews] = useState<DropdownOption>({
+        value: "Newest First",
+        label: "Newest First",
+    });
+    const [categoryOptions, setCategoryOptions] = useState<DropdownOption[]>(
+        []
+    );
+    const [authorOptions, setAuthorOptions] = useState<DropdownOption[]>([]);
 
-  useEffect(function loadAPI() {
-    const getCategoryOptions = (posts: PostData[]): DropdownOption[] => {
-      const unique = Array.from(new Set(posts.map((post) => post.categories)));
-      return [
-        { value: "All Categories", label: "All Categories" },
-        ...unique.map((cat) => ({ value: cat, label: cat })),
-      ];
-    };
+    const filteredPosts = useMemo(() => {
+        return posts
+            .filter((post) => {
+                const matchesSearch = post.title
+                    .toLowerCase()
+                    .includes(searchValue.toLowerCase());
+                const matchesCategory =
+                    selectedCategory.value === "All Categories" ||
+                    selectedCategory.value === post.categories;
+                const matchesAuthor =
+                    selectedAuthors.value === "All Authors" ||
+                    selectedAuthors.value === post.author;
+                return matchesSearch && matchesCategory && matchesAuthor;
+            })
+            .sort((a, b) => {
+                if (selectedNews.value === "Newest First") {
+                    return (
+                        new Date(b.createdAt).getTime() -
+                        new Date(a.createdAt).getTime()
+                    );
+                } else if (selectedNews.value === "Oldest First") {
+                    return (
+                        new Date(a.createdAt).getTime() -
+                        new Date(b.createdAt).getTime()
+                    );
+                } else if (selectedNews.value === "Title A - Z") {
+                    return a.title.localeCompare(b.title);
+                }
+                return 0;
+            });
+    }, [searchValue, selectedCategory, selectedAuthors, selectedNews]);
 
-    const getAuthorOptions = (posts: PostData[]): DropdownOption[] => {
-      const unique = Array.from(new Set(posts.map((post) => post.author)));
-      return [
-        { value: "All Authors", label: "All Authors" },
-        ...unique.map((author) => ({ value: author, label: author })),
-      ];
-    };
+    useEffect(function loadAPI() {
+        const getCategoryOptions = (posts: PostData[]): DropdownOption[] => {
+            const unique = Array.from(
+                new Set(posts.map((post) => post.categories))
+            );
+            return [
+                { value: "All Categories", label: "All Categories" },
+                ...unique.map((cat) => ({ value: cat, label: cat })),
+            ];
+        };
 
-    setCategoryOptions(getCategoryOptions(posts));
-    setAuthorOptions(getAuthorOptions(posts));
-  }, []);
+        const getAuthorOptions = (posts: PostData[]): DropdownOption[] => {
+            const unique = Array.from(
+                new Set(posts.map((post) => post.author))
+            );
+            return [
+                { value: "All Authors", label: "All Authors" },
+                ...unique.map((author) => ({ value: author, label: author })),
+            ];
+        };
 
-  return (
-    <div className="min-h-screen bg-background">
-      <div className="container mx-auto px-4 py-8">
-        <Header />
+        setCategoryOptions(getCategoryOptions(posts));
+        setAuthorOptions(getAuthorOptions(posts));
+    }, []);
 
-        <Filter
-          searchValue={searchValue}
-          categoryOptions={categoryOptions}
-          authorOptions={authorOptions}
-          sortOptions={SORT_OPTIONS}
-          selectedCategory={selectedCategory}
-          selectedAuthors={selectedAuthors}
-          selectedNews={selectedNews}
-          onSearchValueChange={setSearchValue}
-          onSelectCategory={setSelectedCategory}
-          onSelectAuthors={setSelectedAuthors}
-          onSelectNews={setSelectedNews}
-        />
+    const [testState, setTestState] = useState(0);
 
-        <PostList
-          posts={filteredPosts}
-          totalPosts={posts.length}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-background">
+            <div className="container mx-auto px-4 py-8">
+                <Header />
+
+                <button
+                    className="bg-red-500 cursor-pointer"
+                    onClick={() => {
+                        setTestState((prev) => prev + 1);
+                    }}
+                >
+                    test {testState}
+                </button>
+
+                <Filter
+                    searchValue={searchValue}
+                    categoryOptions={categoryOptions}
+                    authorOptions={authorOptions}
+                    sortOptions={SORT_OPTIONS}
+                    selectedCategory={selectedCategory}
+                    selectedAuthors={selectedAuthors}
+                    selectedNews={selectedNews}
+                    onSearchValueChange={setSearchValue}
+                    onSelectCategory={setSelectedCategory}
+                    onSelectAuthors={setSelectedAuthors}
+                    onSelectNews={setSelectedNews}
+                />
+
+                <PostList posts={filteredPosts} totalPosts={posts.length} />
+            </div>
+        </div>
+    );
 };
--- src/pages/posts/post-list/post-list.tsx
+++ src/pages/posts/post-list/post-list.tsx
@@ -1,32 +1,41 @@
 import { type PostData } from "../shared/data/posts.data.ts";
 import { PostCard } from "../post-list/post-detail-card.tsx";
+import { memo } from "react";
 
-export const PostList = ({
-  posts = [],
-  totalPosts = 0,
-}: {
-  posts: PostData[];
-  totalPosts: number;
-}) => {
-  return (
-    <>
-      <p className="mb-6 text-sm text-muted-foreground">
-        Showing {posts.length} of {totalPosts} posts
-      </p>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {posts.map((post, index) => (
-          <PostCard
-            key={index}
-            thumbnail={post.thumbnailURL}
-            title={post.title}
-            content={post.content}
-            author={post.author}
-            categories={post.categories}
-            createdAt={post.createdAt}
-            lastReadAt={post.lastReadAt}
-          />
-        ))}
-      </div>
-    </>
-  );
-};
+export const PostList = memo(
+    ({
+        posts = [],
+        totalPosts = 0,
+    }: {
+        posts: PostData[];
+        totalPosts: number;
+    }) => {
+        return (
+            <>
+                <p className="mb-6 text-sm text-muted-foreground">
+                    Showing {posts.length} of {totalPosts} posts
+                </p>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {posts.map((post, index) => (
+                        <PostCard
+                            key={index}
+                            thumbnail={post.thumbnailURL}
+                            title={post.title}
+                            content={post.content}
+                            author={post.author}
+                            categories={post.categories}
+                            createdAt={post.createdAt}
+                            lastReadAt={post.lastReadAt}
+                        />
+                    ))}
+                </div>
+            </>
+        );
+    },
+    (prevProps, nextProps) => {
+        return (
+            prevProps.posts === nextProps.posts &&
+            prevProps.totalPosts === nextProps.totalPosts
+        );
+    }
+);
