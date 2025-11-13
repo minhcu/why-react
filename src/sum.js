d419f84c9309435de00cc6d087cb4f43c6806a77
minhcu <minhdong1831999@gmail.com>
2025-11-13 20:58:44 +0700

    update: CI

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,8 @@
 function sum(a, b) {
-
+    if (typeof a === undefined || typeof b === 'undefined') {
+        return new Error('Both arguments are required');
+    }
+    
     return a + b;
 }
 module.exports = {
